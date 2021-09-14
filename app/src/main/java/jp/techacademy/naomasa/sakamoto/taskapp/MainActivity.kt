893fd1986a7cd9635114de87b556b92644a1b679
sakamona <sakamona@cocoto.co.jp>
2021-09-14 12:18:52 +0900

    検索機能追加

--- app/src/main/java/jp/techacademy/naomasa/sakamoto/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/naomasa/sakamoto/taskapp/MainActivity.kt
@@ -10,23 +10,32 @@ import android.content.Intent
 import androidx.appcompat.app.AlertDialog
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.util.Log
+import androidx.appcompat.widget.SearchView
+import io.realm.RealmResults
+import kotlinx.android.synthetic.main.content_input.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mRealm: Realm
     private val mRealmListener = object : RealmChangeListener<Realm> {
         override fun onChange(element: Realm) {
-            reloadListView()
+            // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
+            val allViewResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+            reloadListView(allViewResults)
         }
     }
 
     private lateinit var mTaskAdapter: TaskAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab.setOnClickListener { view ->
+
+        fab.setOnClickListener {
             val intent = Intent(this, InputActivity::class.java)
             startActivity(intent)
         }
@@ -37,6 +46,32 @@ class MainActivity : AppCompatActivity() {
 
         // ListViewの設定
         mTaskAdapter = TaskAdapter(this)
+
+        //検索する時
+        // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
+        val allViewResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+//        search.isSubmitButtonEnabled = true
+
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                // text changed
+                val searchViewResults =
+                    mRealm.where(Task::class.java).equalTo("category", newText).findAll()
+                        .sort("date", Sort.DESCENDING)
+                reloadListView(searchViewResults)
+                //これがないと検索中、検索文字がないとカテゴリがないものが検索されていることになってしまう
+                if (newText == "") {
+                    reloadListView(allViewResults)
+                }
+                return false
+            }
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // submit button pressed
+//                val searchViewResults = mRealm.where(Task::class.java).equalTo("category", query).findAll().sort("date", Sort.DESCENDING)
+//                reloadListView(searchViewResults)
+                return false
+            }
+        })
 
         // ListViewをタップしたときの処理
         listView1.setOnItemClickListener { parent, _, position, _ ->
@@ -76,7 +111,9 @@ class MainActivity : AppCompatActivity() {
                 val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
                 alarmManager.cancel(resultPendingIntent)
 
-                reloadListView()
+                //検索中の削除の場合は、検索文字のリセット
+                search.setQuery(null, false)
+                reloadListView(allViewResults)
             }
 
             builder.setNegativeButton("CANCEL", null)
@@ -87,16 +124,13 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        reloadListView()
+        reloadListView(allViewResults)
     }
 
-    private fun reloadListView() {
-        //if(検索された)
-        // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
-        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+    private fun reloadListView(viewResult: MutableList<Task>) {
 
         // 上記の結果を、TaskListとしてセットする
-        mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+        mTaskAdapter.mTaskList = mRealm.copyFromRealm(viewResult)
 
         // TaskのListView用のアダプタに渡す
         listView1.adapter = mTaskAdapter
@@ -114,4 +148,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_TASK = "jp.techacademy.naomasa.sakamoto.taskapp.TASK"
     }
+
+
 }
